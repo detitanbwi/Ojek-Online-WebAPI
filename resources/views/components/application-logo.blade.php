543abefee8f8cf2f3ed37d867ac5d3d2d41ebcd3
Titan Anggaraksa <detitanbwi@gmail.com>
2026-07-06 15:31:18 +0700

    feat: generate transparent, white, and black logo variants and use white logo in dark navbar

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,12 @@
-<img src="{{ asset('favicon.png') }}" {{ $attributes }} alt="Wirojek Logo" />
+@props(['type' => 'original'])
+
+@php
+$src = match($type) {
+    'white' => asset('logo-white.png'),
+    'black' => asset('logo-black.png'),
+    'transparent' => asset('logo-transparent.png'),
+    default => asset('favicon.png'),
+};
+@endphp
+
+<img src="{{ $src }}" {{ $attributes }} alt="Wirojek Logo" />
